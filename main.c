e6154cb4f14a13d613af88df596341c4c836a614
Victor Hugo <victorferrarihugo@hotmail.com>
2024-07-13 22:21:23 -0300

    Adicionando jogada

--- main.c
+++ main.c
@@ -20,7 +20,12 @@ void inicia_tabuleiro (struct tabuleiro *tabuleiro) {
 
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            printf("%c |", tabuleiro->matriz[i][j]);
+            if (j != 2) {
+                printf("%c |", tabuleiro->matriz[i][j]); // Correção do tabuleiro
+            }
+            else {
+                printf("%c", tabuleiro->matriz[i][j]);
+            }
         }
     printf("\n");
     }
@@ -34,8 +39,31 @@ void registra_jogadores (struct jogadores *jogadores) {
     printf("Agora digite o nome do %d° jogador: ", 2);
     scanf(" %s", jogadores[1].nome);
     jogadores[1].simbolo = 'O';
+
 }
 
+
+void mostra_tabuleiro (struct tabuleiro *t) {
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (j != 2) {
+                printf("%c|", t->matriz[i][j]); 
+            }
+            else {
+                printf("%c", t->matriz[i][j]);
+            }
+        }
+    printf("\n");
+    }
+}
+
+void registra_jogada (struct tabuleiro *t, struct jogadores *p) {
+    int i, j;
+    printf("Onde gostaria que fosse sua jogada, jogador %d? ", 1);
+    scanf("%d %d", &i, &j);
+
+    t->matriz[i][j] = p[0].simbolo;
+}
 
 int main () {
     struct tabuleiro t;
@@ -43,5 +71,6 @@ int main () {
     
     inicia_tabuleiro(&t);
     registra_jogadores(player);
-
+    registra_jogada(&t, player);
+    mostra_tabuleiro(&t);
 }
