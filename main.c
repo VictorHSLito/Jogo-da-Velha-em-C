24c3f50db4c3a7856e01733fe0e4f83c04771cc5
Victor Hugo <victorferrarihugo@hotmail.com>
2024-07-13 22:11:53 -0300

    Adicionando os jogadores

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
 
-
 struct tabuleiro {
     char matriz[3][3];
 };
 
+
+struct jogadores {
+    char nome[30];
+    char simbolo;
+};
 
 void inicia_tabuleiro (struct tabuleiro *tabuleiro) {
     for (int i = 0; i < 3; i++) {
@@ -23,8 +27,21 @@ void inicia_tabuleiro (struct tabuleiro *tabuleiro) {
 
 }
 
+void registra_jogadores (struct jogadores *jogadores) {
+    printf("Digite o nome do %d° jogador: ", 1);
+    scanf(" %s", jogadores[0].nome);
+    jogadores[0].simbolo = 'X';
+    printf("Agora digite o nome do %d° jogador: ", 2);
+    scanf(" %s", jogadores[1].nome);
+    jogadores[1].simbolo = 'O';
+}
+
+
 int main () {
     struct tabuleiro t;
+    struct jogadores player[2];
     
     inicia_tabuleiro(&t);
+    registra_jogadores(player);
+
 }
